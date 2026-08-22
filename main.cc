ab58ac96bd9a49298efa2a926da34b074f3a15e0
Ching-btw <0000ayush17@gmail.com>
2026-08-23 01:24:17 +0530

    detect normals on the sphere

--- main.cc
+++ main.cc
@@ -5,17 +5,23 @@
 #include <iostream>
 #include <fstream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
     double a = dot(r.direction(), r.direction());
     double b = -2.0 * dot(r.direction(), oc);
     double c = dot(oc, oc) - radius * radius;
     double discriminant = b * b - 4 * a * c;
-    return discriminant >= 0;
+    
+    if(discriminant < 0) return -1.0;
+    else return (-b - std::sqrt(discriminant)) / (2.0 * a);
 }
 
 color ray_color(const ray& ray) {
-    if(hit_sphere(point3(0, 0, -1), 0.5, ray)) return color(1, 0, 0);
+    double t = hit_sphere(point3(0, 0, -1), 0.5, ray);
+    if(t > 0.0){
+        vec3 N = unit_vector(ray.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
     vec3 unit_dir = unit_vector(ray.direction());
     double a = 0.5 * (unit_dir.y() + 1.0);
