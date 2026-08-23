1cece407600a0066276a0f7dc3995d9b1f7de36f
Ching-btw <0000ayush17@gmail.com>
2026-08-24 01:44:50 +0530

    add sphere class

--- ray.h
+++ ray.h
@@ -7,17 +7,17 @@ class ray {
     public:
         ray() {}
 
-        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+        ray(const point3& origin, const vec3& direction) : origin(origin), dir(direction) {}
 
-        const point3& origin() const { return orig; }
+        const point3& origin() const { return origin; }
         const vec3& direction() const { return dir; }
 
         point3 at(double t) const {
-            return orig + t * dir;
+            return origin + t * dir;
         }
 
     private:
-        point3 orig;
+        point3 origin;
         vec3 dir;
 };
 
