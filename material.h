f1e41687bdbdf1619f36ed17270480ab8bacb268
Ching-btw <0000ayush17@gmail.com>
2026-09-12 07:54:31 +0530

    model light scattering an reflection for lambertian mats

--- material.h
+++ material.h
@@ -12,4 +12,22 @@ class material {
         }
 };
 
+class lambertian : public material {
+    public:
+        lambertian(const color& albedo) : albedo(albedo) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            vec3 scattered_direction = rec.normal + random_unit_vector();
+
+            if(scattered_direction.near_zero()) scattered_direction = rec.normal;
+
+            scattered = ray(rec.p, scattered_direction);
+            attenuation = albedo;
+             return true;
+        }
+
+    private:
+        color albedo;
+};
+
 #endif
