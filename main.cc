f86fad38c9d83fea67ea3106a860ead1602f4880
Ching-btw <0000ayush17@gmail.com>
2026-09-12 12:51:15 +0530

    blue air inside hollow sphere because why not ?

--- main.cc
+++ main.cc
@@ -18,7 +18,7 @@ int main() {
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(color(0.7, 1.0, 0.7), 1.50);
-    auto material_bubble = make_shared<dielectric>(color(1.0, 1.0, 1.0), 1.0 / 1.50);
+    auto material_bubble = make_shared<dielectric>(color(0.7, 0.7, 1.0), 1.0 / 1.50);
     auto material_right  = make_shared<metal>(color(0.8, 0.8, 0.4), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
