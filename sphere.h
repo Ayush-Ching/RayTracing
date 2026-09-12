2aef7ba7bc36eaf955bb7c93880af1ffde44ea6d
Ching-btw <0000ayush17@gmail.com>
2026-09-12 07:43:18 +0530

    add mat info to hittables

--- sphere.h
+++ sphere.h
@@ -5,7 +5,9 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {
+            // init mat here later
+        }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
@@ -28,6 +30,7 @@ class sphere : public hittable {
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
 
             return true;
         }
@@ -35,6 +38,7 @@ class sphere : public hittable {
     private:
         point3 center;
         double radius;
+        shared_ptr<material> mat;
 };
 
 #endif
